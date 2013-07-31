0cf60f84d88954183252f55d8ab7e278028e73d2
Chas Emerick <chas@cemerick.com>
2013-07-31 07:45:48 -0400

    recommended cljs rev + configuration for cljs repl, piggieback

--- project.clj
+++ project.clj
@@ -4,15 +4,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [pathetic "0.5.0"]]
+                 [pathetic "0.5.0"]
+                 [org.clojure/clojurescript "0.0-1835" :optional true]]
 
   :source-paths ["src" "target/generated-src"]
   :test-paths ["target/generated-test"]
   :aliases  {"cleantest" ["do" "clean," "cljx" "once," "test,"
                           "cljsbuild" "once," "cljsbuild" "test"]}
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]]
+  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
+                                  [com.keminglabs/cljx "0.3.0"]
+                                  [com.cemerick/piggieback "0.0.5"]]
                    :plugins [[lein-cljsbuild "0.3.2"]
-                             [com.keminglabs/cljx "0.3.0"]]}}
+                             [com.keminglabs/cljx "0.3.0"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
+                                                     cljx.repl-middleware/wrap-cljx]}}}
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/generated-src"
                    :rules :clj}
