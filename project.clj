e278dde44f48e88b2b40305baf615d30f3eb6b5b
Chas Emerick <chas@cemerick.com>
2013-07-31 07:31:25 -0400

    minor project cleanup

--- project.clj
+++ project.clj
@@ -4,29 +4,28 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [pathetic "0.5.0"]
-                 [com.cemerick/clojurescript.test "0.0.4"]]
+                 [pathetic "0.5.0"]]
 
-  :source-paths ["src" "generated-src"]
-  :test-paths ["generated-test"]
-  :aliases  {"all" ["with-profile"  "dev"]}
-  :plugins [[com.keminglabs/cljx "0.3.0"]
-            [lein-cljsbuild "0.3.2"]]
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.0"]]
-                   :plugins [[lein-clojars "0.8.0"]]}}
+  :source-paths ["src" "target/generated-src"]
+  :test-paths ["target/generated-test"]
+  :aliases  {"cleantest" ["do" "clean," "cljx" "once," "test,"
+                          "cljsbuild" "once," "cljsbuild" "test"]}
+  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]]
+                   :plugins [[lein-cljsbuild "0.3.2"]
+                             [com.keminglabs/cljx "0.3.0"]]}}
   :cljx {:builds [{:source-paths ["src"]
-                   :output-path "generated-src"
+                   :output-path "target/generated-src"
                    :rules :clj}
                   {:source-paths ["src"]
-                   :output-path "generated-src"
+                   :output-path "target/generated-src"
                    :rules :cljs}
                   {:source-paths ["test"]
-                   :output-path "generated-test"
+                   :output-path "target/generated-test"
                    :rules :clj}
                   {:source-paths ["test"]
-                   :output-path "generated-test"
+                   :output-path "target/generated-test"
                    :rules :cljs}]}
-  :cljsbuild {:builds [{:source-paths ["generated-src" "generated-test"]
+  :cljsbuild {:builds [{:source-paths ["target/generated-src" "target/generated-test"]
                         :compiler {:output-to "target/cljs/testable.js"}
                         :optimizations :whitespace
                         :pretty-print true}]
